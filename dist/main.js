08b7b935774477cab9aa26216bef7e39fb0aa6d3
michelleroos <hello.michelleroos@gmail.com>
2021-04-20 11:54:50 -0400

    Worked through phases 0-2

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,66 @@
-(()=>{const o=document.getElementById("game-canvas");o.width=500,o.height=500,console.log("Webpack is working!"),console.log("Webpack TEST!"),console.log("Webpack is working!"),console.log("Webpack TEST!"),console.log("Webpack is working!"),console.log("Webpack TEST!")})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("const MovingObject = __webpack_require__(/*! ./moving_object.js */ \"./src/moving_object.js\");\n// window.MovingObject = MovingObject;\n\ncanvas: document.addEventListener(\"DOMContentLoaded\", () => {\n    const canvas = document.getElementById(\"game-canvas\");\n    canvas.width = 500;\n    canvas.height = 500;\n\n    const ctx = canvas.getContext(\"2D\");\n\n    const mo = new MovingObject({\n        pos: [30, 30],\n        vel: [10, 10],\n        radius: 5,\n        color: \"#00FF00\"\n    });\n\n    mo.draw(ctx);\n\n});\n\n\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/moving_object.js":
+/*!******************************!*\
+  !*** ./src/moving_object.js ***!
+  \******************************/
+/***/ ((module) => {
+
+eval("function MovingObject(options) {\n    this.pos = options.pos;\n    this.vel = options.vel;\n    this.radius = options.radius;\n    this.color = options.color;\n}\n\nMovingObject.prototype.draw = function(ctx) {\n    ctx.beginPath();\n    ctx.arc(this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, true);\n    ctx.fillStyle = this.color;\n    ctx.fill();\n    // ctx.stroke();\n}\n\nmodule.exports = MovingObject;\n\n\n//# sourceURL=webpack:///./src/moving_object.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-const canvas = document.getElementById("game-canvas")
-canvas.width = 500;
-canvas.height = 500;
+const MovingObject = require("./moving_object.js");
+// window.MovingObject = MovingObject;
+
+canvas: document.addEventListener("DOMContentLoaded", () => {
+    const canvas = document.getElementById("game-canvas");
+    canvas.width = 500;
+    canvas.height = 500;
+
+    const ctx = canvas.getContext("2D");
+
+    const mo = new MovingObject({
+        pos: [30, 30],
+        vel: [10, 10],
+        radius: 5,
+        color: "#00FF00"
+    });
+
+    mo.draw(ctx);
+
+});
+
